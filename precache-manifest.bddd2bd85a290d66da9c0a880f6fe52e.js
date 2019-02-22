621096bd3c5f0b29c2a59936dc7b647237447030
IGAjinkya <47890130+IGAjinkya@users.noreply.github.com>
2019-02-22 18:08:26 +0530

    First Commit

---
+++ precache-manifest.bddd2bd85a290d66da9c0a880f6fe52e.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9054f8243f5cbbb7e6e9f70c90c25e6",
+    "url": "assets/tappyplane/PNG/Planes/planeGreen2.png"
+  },
+  {
+    "revision": "04d369ba620354b26b6b",
+    "url": "game.8f2aa9c54ed2791cdac8.js"
+  },
+  {
+    "revision": "42b184db0c714ac7ee1f31d5aabd966a",
+    "url": "index.html"
+  },
+  {
+    "revision": "fce01705782b2b0cfd332771d6c1fabf",
+    "url": "sw.js"
+  },
+  {
+    "revision": "9c33e52dc85a3b78c863",
+    "url": "vendors.52fbdafd93fa9dfff3a1.js"
+  },
+  {
+    "revision": "d026c8477a154a8fbfe9523feda4ae7e",
+    "url": "assets/tappyplane/PNG/Planes/planeRed1.png"
+  },
+  {
+    "revision": "dee47403321ac39a7bd7c9add2a92c6d",
+    "url": "assets/tappyplane/Vector/vector.svg"
+  },
+  {
+    "revision": "51ee0dd66b9dabd9281d2080f4691647",
+    "url": "assets/tappyplane/license.txt"
+  },
+  {
+    "revision": "f8e936e5a8372eb461f325b89b22ce66",
+    "url": "assets/tappyplane/preview.png"
+  },
+  {
+    "revision": "4e60451f8437337d1fdc0d7cacf277f5",
+    "url": "assets/tappyplane/sample.png"
+  },
+  {
+    "revision": "e08eebd1844496f1632cd68d97c52856",
+    "url": "assets/tappyplane/Vector/vector.swf"
+  },
+  {
+    "revision": "ad3c217a309a7938593620cc4c8316d2",
+    "url": "assets/tappyplane/PNG/Planes/planeRed3.png"
+  },
+  {
+    "revision": "8c285577176a5e5ea290e4e66f7adf92",
+    "url": "assets/tappyplane/PNG/groundDirt.png"
+  },
+  {
+    "revision": "2ab7f3004eafe3cecaf19d53742d46e4",
+    "url": "assets/tappyplane/Spritesheet/sheet.png"
+  },
+  {
+    "revision": "c915e584ad0cb43b9eceae65d1e17fe3",
+    "url": "assets/tappyplane/PNG/groundRock.png"
+  },
+  {
+    "revision": "e6c3f1f9f7dee360cb4368e7aaa7848d",
+    "url": "assets/tappyplane/PNG/groundGrass.png"
+  },
+  {
+    "revision": "5e6afdf8ad9d249463b7744b4915ffd6",
+    "url": "assets/tappyplane/Spritesheet/planes.png"
+  },
+  {
+    "revision": "ae41b7003b91b3dd1bbe2f4a1dc89513",
+    "url": "assets/tappyplane/Spritesheet/sheet.xml"
+  },
+  {
+    "revision": "8133e772d0c8541fc51a01f4bb17bdc7",
+    "url": "assets/tappyplane/Spritesheet/planes.xml"
+  },
+  {
+    "revision": "c104d31e2dac3ad744f3aa5a4414658c",
+    "url": "assets/tappyplane/PNG/starSilver.png"
+  },
+  {
+    "revision": "7ad8be00a6daa1bdbc9b4adcb20f21ac",
+    "url": "assets/tappyplane/PNG/groundSnow.png"
+  },
+  {
+    "revision": "e49d0967e6027af94138b487a6302d8b",
+    "url": "assets/tappyplane/PNG/starGold.png"
+  },
+  {
+    "revision": "17b91223b54d250a63af2641e3a0402b",
+    "url": "assets/tappyplane/PNG/starBronze.png"
+  },
+  {
+    "revision": "b44a22896dab6c51cc74c4d5eac57a39",
+    "url": "assets/tappyplane/PNG/rockSnowDown.png"
+  },
+  {
+    "revision": "8d523d1fd458e290585a1b2d6a107415",
+    "url": "assets/tappyplane/PNG/rockSnow.png"
+  },
+  {
+    "revision": "becb76fcc1481faaee2a1da7d33fd0e7",
+    "url": "assets/tappyplane/PNG/rockIceDown.png"
+  },
+  {
+    "revision": "1812ba33780c8aeddf507d97a84bf4c6",
+    "url": "assets/tappyplane/PNG/rockIce.png"
+  },
+  {
+    "revision": "4bc830cde062589676d22a79ba9f2d4b",
+    "url": "assets/tappyplane/PNG/rockGrassDown.png"
+  },
+  {
+    "revision": "2033d1ed35da61940ce101927f2863e2",
+    "url": "assets/tappyplane/PNG/rockGrass.png"
+  },
+  {
+    "revision": "9894933d326e972ec731832e73409bfa",
+    "url": "assets/tappyplane/PNG/rockDown.png"
+  },
+  {
+    "revision": "87acdffd268f0113e6248f80974659a4",
+    "url": "assets/tappyplane/PNG/rock.png"
+  },
+  {
+    "revision": "f9525bbcb018606ac416190bc0f966ba",
+    "url": "assets/tappyplane/PNG/puffSmall.png"
+  },
+  {
+    "revision": "d4fc5a4711aa4f49a883ad5298d1957f",
+    "url": "assets/tappyplane/PNG/puffLarge.png"
+  },
+  {
+    "revision": "834fff409cfaee3b8a343185415ef15a",
+    "url": "assets/tappyplane/PNG/groundIce.png"
+  },
+  {
+    "revision": "b1f288271bb909a5c9ff7e630880ce85",
+    "url": "assets/tappyplane/PNG/Letters/letterQ.png"
+  },
+  {
+    "revision": "950a1b762c069ad67607ee1a9a01d18d",
+    "url": "assets/tappyplane/PNG/Letters/letterZ.png"
+  },
+  {
+    "revision": "43cde7d5c2a0d0c8d9f10b08cef442d9",
+    "url": "assets/tappyplane/PNG/Numbers/number1.png"
+  },
+  {
+    "revision": "29c10a403f4e5ec294fbb2e0e4af5ce0",
+    "url": "assets/tappyplane/PNG/Numbers/number2.png"
+  },
+  {
+    "revision": "1260f71ed6e5ace2a37a08e6bc5203d3",
+    "url": "assets/tappyplane/PNG/Numbers/number3.png"
+  },
+  {
+    "revision": "0c5fe112ccf0628c32d217cdcfc85e4c",
+    "url": "assets/tappyplane/PNG/Numbers/number4.png"
+  },
+  {
+    "revision": "0942273599181de7d517771a2b389e13",
+    "url": "assets/tappyplane/PNG/Numbers/number5.png"
+  },
+  {
+    "revision": "0235e631355bfbcf7656c8b52c634462",
+    "url": "assets/tappyplane/PNG/Numbers/number6.png"
+  },
+  {
+    "revision": "c616dd9d55be2e9de29369ae5113ff56",
+    "url": "assets/tappyplane/PNG/Numbers/number7.png"
+  },
+  {
+    "revision": "375f29c1d0341207cc8b73dc20290a90",
+    "url": "assets/tappyplane/PNG/Numbers/number8.png"
+  },
+  {
+    "revision": "958257b0b7fd47e0d239992b530c8c5a",
+    "url": "assets/tappyplane/PNG/Numbers/number9.png"
+  },
+  {
+    "revision": "1fde7eb7dfd90e68eecd9c0d35a28c5a",
+    "url": "assets/tappyplane/PNG/UI/textGameOver.png"
+  },
+  {
+    "revision": "036dc9974ff879afcc0e49a31013187d",
+    "url": "assets/tappyplane/PNG/UI/textGetReady.png"
+  },
+  {
+    "revision": "ef06f4241e4a4389e2b838451b96f27c",
+    "url": "assets/tappyplane/PNG/Planes/planeBlue3.png"
+  },
+  {
+    "revision": "afd0b881a0c3086fa3d13844445dcf87",
+    "url": "assets/tappyplane/PNG/Planes/planeGreen1.png"
+  },
+  {
+    "revision": "a13ef5422699dd54643b6211ee8d85eb",
+    "url": "assets/tappyplane/PNG/UI/medalBronze.png"
+  },
+  {
+    "revision": "90194d6fb6acfefa27f1064893ee724e",
+    "url": "assets/tappyplane/PNG/Planes/planeBlue2.png"
+  },
+  {
+    "revision": "9cca6fc358bd4cbe5c23c122dde7a039",
+    "url": "assets/tappyplane/PNG/Planes/planeGreen3.png"
+  },
+  {
+    "revision": "93b740a1f3c69642f0a92c8eb446600c",
+    "url": "assets/tappyplane/PNG/UI/medalGold.png"
+  },
+  {
+    "revision": "934785b0f091b0751b74f0ed7140e2b2",
+    "url": "assets/tappyplane/PNG/Planes/planeBlue1.png"
+  },
+  {
+    "revision": "dec847172f5a14c9ebf45ee6bf488389",
+    "url": "assets/tappyplane/PNG/Planes/planeYellow1.png"
+  },
+  {
+    "revision": "7c550bf22be6a50a849bae54dfcd20a8",
+    "url": "assets/tappyplane/PNG/background.png"
+  },
+  {
+    "revision": "099b9ccf06412a905e0dadf1cf9112e7",
+    "url": "assets/tappyplane/PNG/Planes/planeYellow2.png"
+  },
+  {
+    "revision": "d79fe91e8878b4102f784763e89d12ea",
+    "url": "assets/tappyplane/PNG/Planes/planeRed2.png"
+  },
+  {
+    "revision": "9dde67049157aff9bf0f4feaecaee80f",
+    "url": "assets/tappyplane/PNG/Planes/planeYellow3.png"
+  },
+  {
+    "revision": "ce3eb62fe69b30dbb23a5780944131d4",
+    "url": "assets/tappyplane/PNG/UI/UIbg.png"
+  },
+  {
+    "revision": "40b8d660ecd98305307312c57a127b5a",
+    "url": "assets/tappyplane/PNG/UI/tapLeft.png"
+  },
+  {
+    "revision": "e0308927b4201daeaf5033f74521a7b4",
+    "url": "assets/tappyplane/PNG/UI/tap.png"
+  },
+  {
+    "revision": "6318e30e20bf2cdda4ad1a4a1aa77514",
+    "url": "assets/tappyplane/PNG/UI/tapTick.png"
+  },
+  {
+    "revision": "0a61e3a285392a2b20b33fda862705d5",
+    "url": "assets/tappyplane/PNG/UI/medalSilver.png"
+  },
+  {
+    "revision": "aec02ab6d519582b46606c29f4015435",
+    "url": "assets/tappyplane/PNG/UI/tapRight.png"
+  },
+  {
+    "revision": "5775c14d7cfe6fd8a3dde359a8309be2",
+    "url": "assets/tappyplane/PNG/UI/buttonSmall.png"
+  },
+  {
+    "revision": "7129df6b9ae3f77438d0a7bd96afb661",
+    "url": "assets/tappyplane/PNG/Numbers/number0.png"
+  },
+  {
+    "revision": "1d7c07796a229ebbd5acafb3a86ec4b6",
+    "url": "assets/tappyplane/PNG/UI/buttonLarge.png"
+  },
+  {
+    "revision": "98be65d996168cd18c93f40ac56e2437",
+    "url": "assets/tappyplane/PNG/Letters/letterW.png"
+  },
+  {
+    "revision": "42d678bb705d4d35661bbbe35d142dea",
+    "url": "assets/tappyplane/PNG/Letters/letterN.png"
+  },
+  {
+    "revision": "b9985370c5d777e0929ebccb647594f5",
+    "url": "assets/tappyplane/PNG/Letters/letterU.png"
+  },
+  {
+    "revision": "aaea467586f37f0118556cb7c0e2bfde",
+    "url": "assets/tappyplane/PNG/Letters/letterP.png"
+  },
+  {
+    "revision": "630099d0bcdbbf232c7294a81c1ce0c3",
+    "url": "assets/tappyplane/PNG/Letters/letterX.png"
+  },
+  {
+    "revision": "9738e19a4ae339586bf26654b5c424b7",
+    "url": "assets/tappyplane/PNG/Letters/letterR.png"
+  },
+  {
+    "revision": "4023fe5f64425b4ec874697239b632e7",
+    "url": "assets/tappyplane/PNG/Letters/letterO.png"
+  },
+  {
+    "revision": "0e893704711e0aa1f451cbc8080a6cb9",
+    "url": "assets/tappyplane/PNG/Letters/letterT.png"
+  },
+  {
+    "revision": "1c9a022b26866852c57406354b909364",
+    "url": "assets/tappyplane/PNG/Letters/letterS.png"
+  },
+  {
+    "revision": "ccd701c046de84f845d479edcf1deff7",
+    "url": "assets/tappyplane/PNG/Letters/letterY.png"
+  },
+  {
+    "revision": "1c20804090766bf228aef7a2c21f4547",
+    "url": "assets/tappyplane/PNG/Letters/letterV.png"
+  },
+  {
+    "revision": "7310b00c820e42548c392f6c471ab683",
+    "url": "assets/tappyplane/Font/kenvector_future.ttf"
+  },
+  {
+    "revision": "5cf196b78a42a85c9ce0c3175bd8038a",
+    "url": "assets/tappyplane/PNG/Letters/letterI.png"
+  },
+  {
+    "revision": "e9c1e5bc2e7e68d68b42847841bf776c",
+    "url": "assets/tappyplane/PNG/Letters/letterJ.png"
+  },
+  {
+    "revision": "c1890728325a1e50001ffec172234385",
+    "url": "assets/tappyplane/PNG/Letters/letterL.png"
+  },
+  {
+    "revision": "952985ff776f996324dbbf2da1783125",
+    "url": "assets/tappyplane/PNG/Letters/letterM.png"
+  },
+  {
+    "revision": "2e6c35c74afdfcf598befbff1e0b2af1",
+    "url": "assets/tappyplane/PNG/Letters/letterK.png"
+  },
+  {
+    "revision": "2768d05692d0d27e3b1a25bba4cc733f",
+    "url": "assets/tappyplane/PNG/Letters/letterA.png"
+  },
+  {
+    "revision": "96b0cb03330db6edcdcd48e8b3b36086",
+    "url": "assets/tappyplane/PNG/Letters/letterC.png"
+  },
+  {
+    "revision": "76a9587cc5587f516eae5fb6ca9b291e",
+    "url": "assets/tappyplane/PNG/Letters/letterD.png"
+  },
+  {
+    "revision": "ac5f73a11c0e64dfc89df2058fbfa06a",
+    "url": "assets/tappyplane/PNG/Letters/letterE.png"
+  },
+  {
+    "revision": "c6cf169b9ee71a913fabbec6c267d768",
+    "url": "assets/tappyplane/PNG/Letters/letterH.png"
+  },
+  {
+    "revision": "b182bb67b4debb5a9a20fab959b7138f",
+    "url": "assets/tappyplane/PNG/Letters/letterG.png"
+  },
+  {
+    "revision": "b13087f9c32dc951a83d6ea8639d0784",
+    "url": "assets/tappyplane/PNG/Letters/letterF.png"
+  },
+  {
+    "revision": "0a26c8c55151b37f978538b17caa57ff",
+    "url": "assets/tappyplane/PNG/Letters/letterB.png"
+  },
+  {
+    "revision": "354337d09524f59508fa49a53bb4a2fd",
+    "url": "assets/tappyplane/Font/kenvector_future_thin.ttf"
+  },
+  {
+    "revision": "e4e8aac2dfa6538ae58fa8d1abb9234f",
+    "url": "assets/img/portrait-to-landscape.png"
+  },
+  {
+    "revision": "b887c333d5ec8274b78d0851e7b3337b",
+    "url": "assets/img/phaser-logo.png"
+  },
+  {
+    "revision": "8bdcc486cda9b423f50e886f2ddb6604",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "2ffbc23293ee8a797bc61e9c02534206",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "assets/icons/favicon.ico"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.bddd2bd85a290d66da9c0a880f6fe52e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
